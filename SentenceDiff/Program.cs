144502c949b7658d794256d1d29032430802051b
Michael Vietluzhskykh <mishavetl@gmail.com>
2020-04-12 21:11:38 +0300

    add console user interface

--- SentenceDiff/Program.cs
+++ SentenceDiff/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Practice8.SentenceDiff
 {
@@ -6,7 +7,25 @@ namespace Practice8.SentenceDiff
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.Write("Enter the name of the inital input file: ");
+                Text initialText = new Text(File.ReadAllText(Console.ReadLine()));
+
+                Console.Write("Enter the name of the transformed input file: ");
+                Text transformedText = new Text(File.ReadAllText(Console.ReadLine()));
+
+                Diff difference = transformedText.DifferenceComparedTo(initialText);
+                
+                Console.Write("Enter the name of the output file: ");
+                File.WriteAllText(Console.ReadLine(), difference.ToString());
+                
+                Console.WriteLine($"Difference has been successfully written");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to execute: " + e.Message);
+            }
         }
     }
 }
